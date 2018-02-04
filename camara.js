35a0a973842e2ff052b5446aec7621339bad9e51
YairMendoza95 <joseyair.95@gmail.com>
2018-02-04 03:33:53 -0600

    Correccion de erroress con las funciones, se cambiaron a arrow functions

---
+++ camara.js
@@ -0,0 +1,32 @@
+class Camara{
+	constructor(video_id, canvas_id, callback){
+		if(!this.idBrowserValid()) return // Si el navegador no es válido retorna vacío sin iniciar nada y termina la ejecución
+
+		// Se obtiene el id del control para el video y en el atributo src se asigna la URL del video obtenido
+		this.video = document.getElementById(video_id)
+		this.canvas = document.getElementById(canvas_id)
+		this.contexto = this.canvas.getContext('2d') // El contexto es el objeto que tienen los métodos que nos permiten dibujar 
+
+		navigator.webkitGetUserMedia({video: true}, (localMediaStream)=>{
+			this.setVideo(localMediaStream)
+			this.setCanvas()
+			callback()
+		}, function (error) {
+			console.log(error)
+		})
+	}
+
+	setVideo(localMediaStream){
+		this.video.src = window.URL.createObjectURL(localMediaStream)
+	}
+
+	setCanvas(){
+		setInterval(()=>{
+			this.contexto.drawImage(this.video, 0, 0);
+		}, 1000/30)
+	}
+
+	idBrowserValid(){
+		return !!(navigator.getUserMedia || navigator.webkitGetUserMedia) // La doble negación convierte lo que sea que reciba a boolean
+	}
+}
--- main.js
+++ main.js
@@ -1,39 +1,12 @@
 ;(function() {
-	// Para acceder al hardware primero se evalua si propiedad getUserMedia esta disponible
-
-	// La doble negación convierte lo que sea que reciba a booleano
-	if(!!(navigator.getUserMedia || navigator.webkitGetUserMedia)){
-		// Tenemos un navegador moderno que lo soporta
-		
-		//
-		navigator.webkitGetUserMedia({video: true}, function(localMediaStream){
-			// Esta funcion se activa cuando el usuario autoriza el uso de la cámara y cargue la imagen de la cámara
-
-			// Se obtiene el id del control para el video y en el atributo src se asigna la URL del video obtenido
-			var video = document.getElementById('video')
-			video.src = window.URL.createObjectURL(localMediaStream)
-
-			var canvas = document.getElementById('canvas')
-			var contexto = canvas.getContext('2d') // El contexto es el objeto que tienen los métodos que nos permiten dibujar 
-
-			var btnDownload = document.getElementById('snap')
-
-			btnDownload.addEventListener("click", function(){
-				var imagenURL = canvas.toDataURL("image/png")
-				var link = document.getElementById('link-download')
-
-				link.href = imagenURL
-				link.download = "photo.png"
-				link.click()
-			})
-			setInterval(function(){
-				contexto.drawImage(this.video, 0, 0);
-			}, 1000/30)
-		}, function(error){
-			console.log(error);
+	var camara = new Camara('video', 'canvas', function(){
+		var btnDownload = document.getElementById('snap')
+		btnDownload.addEventListener("click", function(){
+			var imagenURL = canvas.toDataURL("image/png")
+			var link = document.getElementById('link-download')
+			link.href = imagenURL
+			link.download = "photo.png"
+			link.click()
 		})
-	}
-	else{
-		alert("No lo soporta");
-	}
+	})
 })()
