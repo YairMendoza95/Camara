3bd681332a5fe53ca89ef7457b43f8e96a27d559
YairMendoza95 <joseyair.95@gmail.com>
2018-02-04 01:45:31 -0600

    Implementacion de la captura de la imagen

--- main.js
+++ main.js
@@ -10,11 +10,22 @@
 			// Esta funcion se activa cuando el usuario autoriza el uso de la cámara y cargue la imagen de la cámara
 
 			// Se obtiene el id del control para el video y en el atributo src se asigna la URL del video obtenido
-			var video = document.getElementById('video');
-			video.src = window.URL.createObjectURL(localMediaStream);
+			var video = document.getElementById('video')
+			video.src = window.URL.createObjectURL(localMediaStream)
 
-			var canvas = document.getElementById('canvas');
-			var contexto = canvas.getContext('2d'); // El contexto es el objeto que tienen los métodos que nos permiten dibujar 
+			var canvas = document.getElementById('canvas')
+			var contexto = canvas.getContext('2d') // El contexto es el objeto que tienen los métodos que nos permiten dibujar 
+
+			var btnDownload = document.getElementById('snap')
+
+			btnDownload.addEventListener("click", function(){
+				var imagenURL = canvas.toDataURL("image/png")
+				var link = document.getElementById('link-download')
+
+				link.href = imagenURL
+				link.download = "photo.png"
+				link.click()
+			})
 			setInterval(function(){
 				contexto.drawImage(this.video, 0, 0);
 			}, 1000/30)
